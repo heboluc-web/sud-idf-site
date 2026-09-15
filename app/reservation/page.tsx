4f574a79a62c4fe44dc151af0c719f3a03d33755
Lucas Chamberlain <contact@sudidfexecutivetransport.fr>
2026-09-15 12:03:41 +0200

    fix: update Paris Beauvais fare

--- app/reservation/page.tsx
+++ app/reservation/page.tsx
@@ -391,7 +391,7 @@ export default function Reservation() {
               cdgOrly: estNuit ? 160 : 140,
               parisDisney: estNuit ? 160 : 140,
               parisVersailles: estNuit ? 140 : 120,
-              parisBeauvais: estNuit ? 240 : 210,
+              parisBeauvais: 245,
               parisChantilly: estNuit ? 240 : 210,
             }
           : {
@@ -400,7 +400,7 @@ export default function Reservation() {
               cdgOrly: estNuit ? 140 : 120,
               parisDisney: estNuit ? 140 : 120,
               parisVersailles: estNuit ? 120 : 100,
-              parisBeauvais: estNuit ? 210 : 180,
+              parisBeauvais: 245,
               parisChantilly: estNuit ? 210 : 180,
             };
 
